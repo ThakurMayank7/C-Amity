cc28d3898b44489e55002ec9bf56e44933eb97e5
Mayank <mayanksingh8181335335@gmail.com>
2024-11-09 00:55:31 +0530

    cleanup

--- 4nov2024-set/3.c
+++ 4nov2024-set/3.c
@@ -7,20 +7,16 @@ int main(){
     printf("Enter the number\n");
     scanf("%d", &n);
     int first;
-
     while (1)
     {
-        printf("iterating%d\n", n);
         if (n % 10 < 0 || n % 10 == 0)  //checking when the first digit is reached and breakign
         {
             break;
         }
-
         first = n % 10; //storing the last digit of the current number (if condition is met it will be the first digit of the actual number)
         n /= 10;
     }
     printf("The first digit of the current number is %d\n", first);
     printf("The last digit of the current number is %d\n", n % 10);
-
     return 0;
 }
