df699d95b55c9ea0baef537f28a9a3299c9701dc
Mayank <mayanksingh8181335335@gmail.com>
2024-11-09 00:30:17 +0530

    completed a problem

--- 4nov2024-set/2.c
+++ 4nov2024-set/2.c
@@ -1,6 +1,16 @@
+// Write a program to print sum of all even numbers form 1 to 10
+
 #include <stdio.h>
 
 int main(){
-    
+    int sum = 0;
+    for (int i = 1; i <=10; i++)
+    {
+        if(i%2==0)  //cheking even numbers
+        {
+            sum+=i;   //if even adding it to sum variable
+        }
+    }
+    printf("The sum of even numbers from 1 to 10 is : %d\n",sum);
     return 0;
 }
