0d28c865334f1da2e8f9eaf4bd291798c52d3429
Mayank <mayanksingh8181335335@gmail.com>
2024-11-09 00:53:53 +0530

    completed a problem --excellent

--- 4nov2024-set/3.c
+++ 4nov2024-set/3.c
@@ -6,15 +6,21 @@ int main(){
     int n;
     printf("Enter the number\n");
     scanf("%d", &n);
-    int length;
-for (int i = n; i >0 ; i/=10)
-{
-    
-}
+    int first;
 
+    while (1)
+    {
+        printf("iterating%d\n", n);
+        if (n % 10 < 0 || n % 10 == 0)  //checking when the first digit is reached and breakign
+        {
+            break;
+        }
 
-    printf("The first digit of the number is : %d\n");
-    
-    
+        first = n % 10; //storing the last digit of the current number (if condition is met it will be the first digit of the actual number)
+        n /= 10;
+    }
+    printf("The first digit of the current number is %d\n", first);
+    printf("The last digit of the current number is %d\n", n % 10);
+
     return 0;
 }
