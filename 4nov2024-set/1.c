3b9d29607da01ddac8712eb4a11b1ab5853381f7
Mayank <mayanksingh8181335335@gmail.com>
2024-11-09 00:24:34 +0530

    added a program

--- 4nov2024-set/1.c
+++ 4nov2024-set/1.c
@@ -1,6 +1,18 @@
+// Write a program to print all natural numbers in reverse order from n to 1 using while loop
+
 #include <stdio.h>
 
 int main(){
+    int n;
+    printf("Enter the value of n : \n");
+    scanf("%d", &n);
+
+    printf("The numbers from n to 1 in reverse order are : \n");
+    while(n>0)
+    {
+        printf("%d\n",n);
+        n--;
+    }
     
     return 0;
 }
