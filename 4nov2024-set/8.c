9b666d1ce1e8a4581f6f234a78752c449e9eac40
Mayank <mayanksingh8181335335@gmail.com>
2024-11-11 01:03:19 +0530

    solved

---
+++ 4nov2024-set/8.c
@@ -0,0 +1,31 @@
+//  Write a program to print fibonacci series upto n terms
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter value of n\n");
+    scanf("%d", &n);
+    if(n>=1)
+    {
+        printf("1,");
+        
+    }
+    if (n>=2)
+    {
+        printf("1,");
+    }
+    int a=1,b=1;
+    for(int i=3;i<=n;i++){
+        int c=a+b;
+        if(i==n)    //to avoid the ending comma
+        {
+            printf("%d",c);
+            break;
+        }
+        printf("%d,",c);
+        a=b;
+        b=c;
+    }
+    return 0;
+}
