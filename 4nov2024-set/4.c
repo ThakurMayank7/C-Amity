a64772aa8486b578c908db7e5323979f1f62f22b
Mayank <mayanksingh8181335335@gmail.com>
2024-11-11 00:21:27 +0530

    solved

--- 4nov2024-set/4.c
+++ 4nov2024-set/4.c
@@ -1,6 +1,21 @@
+//   Write a program to check whether the number is palindrome or not
+
 #include <stdio.h>
 
 int main(){
-    
+    int n;
+    printf("Enter the number\n");
+    scanf("%d", &n);
+    int reverse=0;
+    for(int i=n;i>0;i/=10)
+    {
+        reverse=reverse*10+i%10;
+    }
+    if(n==reverse){
+        printf("The number is palindrome\n");
+    }
+    else{
+        printf("The number is not palindrome\n");
+    }
     return 0;
 }
