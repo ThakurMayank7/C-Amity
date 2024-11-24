470ea4cb1c6df82c1cf891d65b84996f018b928f
Mayank <mayanksingh8181335335@gmail.com>
2024-11-24 22:45:21 +0530

    1-a done

---
+++ Lab_File/1/a.c
@@ -0,0 +1,12 @@
+//  Write a C program to add two numbers
+
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("Enter two numbers to add :\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("The sum of numbers is : %d\n",a+b);
+    return 0;
+}
