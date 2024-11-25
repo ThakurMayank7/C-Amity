cb21d730403dba12e8e94d50f6258e435ec83a58
Mayank <mayanksingh8181335335@gmail.com>
2024-11-25 23:02:48 +0530

    10-a done

--- Lab_File/10/a.c
+++ Lab_File/10/a.c
@@ -2,8 +2,23 @@
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
     char character;
-    
+    printf("Enter the character to check whether it is a alphabet or a digit : \n");
+    scanf("%c", &character);
+    int a = (int)character;
+    if (a >= 48 && a <= 57) // checking for a digit
+    {
+        printf("%c is a Digit\n", character);
+    }
+    else if ((a >= 65 && a <= 90) || (a >= 97 && a <= 122)) // checking for a alphabet
+    {
+        printf("%c is a Alphabet\n", character);
+    }
+    else // not alphabet or digit
+    {
+        printf("%c is neither a digit nor a alphabet\n", character);
+    }
     return 0;
 }
