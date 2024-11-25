5e0a3f1a1082a5e050fa970b4bfe11fcfebe1056
Mayank <mayanksingh8181335335@gmail.com>
2024-11-25 23:31:36 +0530

    13 done

---
+++ Lab_File/13.c
@@ -0,0 +1,25 @@
+// Write a program to calculate the root of a Quadratic Equation
+
+#include <stdio.h>
+
+int main()
+{
+    printf("If ax^2+bx+c is the quadratic equation\nEnter the values of a,b and c:\n");
+    float a, b, c;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+
+    float discriminant = (b * b) - (4.0 * a * c);
+    if (discriminant >= 0)
+    {
+        float root1 = (-b + sqrt(discriminant)) / (2.0 * a);
+        float root2 = (-b - sqrt(discriminant)) / (2.0 * a);
+        printf("The roots of the equation are real and distinct: %.2f and %.2f\n", root1, root2);
+    }
+    else
+    {
+        printf("The roots are Imaginary\n");
+    }
+    return 0;
+}
