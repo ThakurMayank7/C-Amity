16eaa87a0a58ce1c1db9d794987c39d80ebe2670
Mayank <mayanksingh8181335335@gmail.com>
2024-11-25 22:55:05 +0530

    9 done

--- Lab_File/10/a.c
+++ Lab_File/10/a.c
@@ -1,8 +1,9 @@
-// 
+// Write a C program to check whether a character is an alphabet, digit
 
 #include <stdio.h>
 
 int main(){
+    char character;
     
     return 0;
 }
--- Lab_File/9.c
+++ Lab_File/9.c
@@ -1,27 +1,31 @@
-//  Write a C program to covert temperature from Fahrenheit to Celsius and Celsius to Fahrenheit (User must provide the choice of type of temperature) 
+//  Write a C program to covert temperature from Fahrenheit to Celsius and Celsius to Fahrenheit (User must provide the choice of type of temperature)
 
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int choice;
     printf("Enter 1 to convert from Celsius to Fahrenheit\nEnter 2 to convert from Fahrenheit to Celsius\n");
     scanf("%d", &choice);
     float temperature;
-    printf("Enter temperature\n");
-    scanf("%f", &temperature);
 
-    if(choice==1){
-        printf("The temperature in Fahrenheit is : %f\n",(temperature*(9.0/5.0))+32.0);
-        
-    }
-    else if(choice==2)
+    if (choice == 1 || choice == 2)
     {
-        printf("The temperature in Celcius is : %f\n",(temperature-32.0)*(5.0/9.0));
+        printf("Enter temperature\n");
+        scanf("%f", &temperature);
 
-    }
-    else{
-        printf("Please Enter a valid choice\n");
-        
+        if (choice == 1)
+        {
+            printf("The temperature in Fahrenheit is : %f\n", (temperature * (9.0 / 5.0)) + 32.0);
+        }
+        else if (choice == 2)
+        {
+            printf("The temperature in Celcius is : %f\n", (temperature - 32.0) * (5.0 / 9.0));
+        }
+        else
+        {
+            printf("Please Enter a valid choice\n");
+        }
     }
     return 0;
 }
