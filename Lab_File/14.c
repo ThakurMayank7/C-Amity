3400a353041f3c798fffb1d881abf711699d0695
Mayank <mayanksingh8181335335@gmail.com>
2024-11-25 23:41:15 +0530

    14 done

--- Lab_File/14.c
+++ Lab_File/14.c
@@ -2,7 +2,41 @@
 
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
+    float x, y;
+    printf("Enter Coordinates of the point:\n");
+    printf("X-coordinate:\n");
+    scanf("%f", &x);
+    printf("Y-coordinate:\n");
+    scanf("%f", &y);
+    if (x == 0 && y == 0)
+    {
+        printf("The point is at the Origin\n");
+    }
+    else if (x == 0)
+    {
+        printf("The point lies on the Y-axis\n");
+    }
+    else if (y == 0)
+    {
+        printf("The point lies on the X-axis\n");
+    }
+    else if (x > 0 && y > 0)
+    {
+        printf("The point is in 1st Quadrant\n");
+    }
+    else if (x < 0 && y > 0)
+    {
+        printf("The point is in 2nd Quadrant\n");
+    }
+    else if (x < 0 && y < 0)
+    {
+        printf("The point is in 3rd Quadrant\n");
+    }
+    else if (x > 0 && y < 0)
+    {
+        printf("The point is in 4th Quadrant\n");
+    }
     return 0;
 }
