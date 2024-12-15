8399f469301e3cd9871180ac1193350d21195171
Mayank <mayanksingh8181335335@gmail.com>
2024-12-15 15:49:15 +0530

    done 32

--- 32.c
+++ 32.c
@@ -1,6 +1,19 @@
+// Write a program in C to print the following pattern:
+// * * *
+// * * *
+// * * *
+
 #include <stdio.h>
 
-int main(){
-    
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
     return 0;
 }
