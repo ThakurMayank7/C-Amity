7fe5fca3eb8f5334aa83bc1cc45f34622c43686e
Mayank <mayanksingh8181335335@gmail.com>
2024-12-15 15:54:08 +0530

    33 done

--- 33.c
+++ 33.c
@@ -1,6 +1,18 @@
+// Write a program in C to print the following pattern: 
+// 1 2 3 
+// 1 2 3 
+// 1 2 3 
+
 #include <stdio.h>
 
 int main(){
-    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", j+1);
+        }
+        printf("\n");
+    }
     return 0;
 }
