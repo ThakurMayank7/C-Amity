0855f9826b4e752b033b18060d62204380605b22
Mayank <mayanksingh8181335335@gmail.com>
2024-12-15 16:23:39 +0530

    40 done

---
+++ 40.c
@@ -0,0 +1,18 @@
+// Write a program in C to print the following pattern: 
+// 3   
+// 3 2  
+// 3 2 1 
+
+#include <stdio.h>
+
+int main(){
+    for (int i = 3; i >=1; i--)
+    {
+        for (int j = 3; j >=i; j--)
+        {
+            printf("%d\t", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
